b63dc4e74e6cb2e183e753dd28aaed2a003a248b
salialkhayzel <111584408+salialkhayzel@users.noreply.github.com>
2023-12-06 01:16:42 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "MEOW",
+    introTitle: 'Hi cutieful',
+    introDesc: `How are you? <3"`,
+    btnIntro: 'CLICK TO CONTINUE MWAA',
+    title: '"Life can feel overwhelming, but amidst it all, keep shining, cutie! Youve got this. Sending a bunch of positivity your way. Youre amazing! ❤️🥰"',
+    desc: 'Crush moko? pogi ba ako? yesyes😊✨"',
+    btnYes: 'CLICK FOR YES <33',
+    btnNo: 'CLICK FOR NO',
+    question: 'YIEEEEEE ANG CUTE MO TALAGA!! Sunset tayo?',
+    btnReply: 'SEND TO KAI<3',
+    reply: 'YESYESYES',
+    mess: ' 😘😘',
+    messDesc: 'SEND to Kai.',
+    btnAccept: 'Good night <3',
+
+}
